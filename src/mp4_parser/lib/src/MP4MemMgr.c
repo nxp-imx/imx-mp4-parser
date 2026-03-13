66d3eb91e30bf900b405851c2bf772d21489b0a6
Ming Zhou <ming.zhou@nxp.com>
2026-03-13 17:18:22 +0800

    MMFMWK-9594-2 Add SBOM file.
    Add SBOM file.
    
    Signed-off-by: Ming Zhou <ming.zhou@nxp.com>

--- src/mp4_parser/lib/src/MP4MemMgr.c
+++ src/mp4_parser/lib/src/MP4MemMgr.c
@@ -1,11 +1,20 @@
-
 /****************************************************************************
- * MP4MemMgr.c.c
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * Copyright (c) 2007 Freescale Semiconductor Inc.
  * Copyright 2026 NXP
- * SPDX-License-Identifier: BSD-3-Clause
- *
  ****************************************************************************/
 
 #include <stdlib.h>
